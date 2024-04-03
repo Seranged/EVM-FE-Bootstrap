a522903e1157a3e703490a2390ea469e9ae39edf
Seranged <seranged@gmail.com>
2024-04-03 15:31:12 +0100

    update metadata structure in layout.tsx
    https://nextjs.org/docs/app/building-your-application/optimizing/metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,41 @@
 import '@/styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import type React from 'react'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ClientProvider from './clientProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: 'EVM-FE-Boilerplate',
+  description: 'EVM-FE-Boilerplate Description Example',
+  viewport: 'width=device-width, initial-scale=1',
+  robots: 'index, follow',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.seranged.com',
+    title: 'EVM-FE-Boilerplate',
+    description: 'EVM-FE-Boilerplate Description Example',
+  },
+  twitter: {
+    site: '@seranged',
+    creator: '@seranged',
+    title: 'EVM-FE-Boilerplate',
+    description: 'EVM-FE-Boilerplate Description Example',
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
-    <>
-      <head>
-        <title>EVM-FE-Boilerplate</title>
-        <meta name='description' content='EVM-FE-Boilerplate' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' type='image/png' sizes='64x64' href='/favicon.png' />
-      </head>
-      <html lang='en'>
-        <body className={`${inter.className}`}>
-          <ClientProvider>{children}</ClientProvider>
-        </body>
-      </html>
-    </>
+    <html lang='en'>
+      <body className={`${inter.className}`}>
+        <ClientProvider>{children}</ClientProvider>
+      </body>
+    </html>
   )
 }
